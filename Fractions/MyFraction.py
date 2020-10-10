7859dde3f9345171f3af005068a5fd89c0825ec7
Rhett Fereday <gmfereday@gmail.com>
2020-10-10 15:16:30 -0500

    Initial commit for git tutorial

---
+++ Fractions/MyFraction.py
@@ -0,0 +1,24 @@
+class MyFraction:
+	def __init__(self, numerator, denominator):
+		self.numerator = numerator
+		self.denominator = denominator
+
+	def __str__(self):
+		return self.numerator + "/" + self.denominator
+
+	def __add__(self, fraction):
+		answer = "Calculate the answer. The answer will be a fraction"
+		return answer
+
+	def __sub__(self, fraction):
+		answer = "Calculate the answer. The answer will be a fraction"
+		return answer
+
+	def __mul__(self, fraction):
+		answer = "Calculate the answer. The answer will be a fraction"
+		return answer
+
+	def __truediv__(self, fraction):
+		answer = "Calculate the answer. The answer will be a fraction"
+		return answer
+
---
+++ Fractions/Test.py
@@ -0,0 +1,17 @@
+import MyFraction
+
+def main():
+	fraction1 = Fraction(1,2)
+	fraction2 = Fraction(1,2)
+
+	#addResult = fraction1 + fraction2
+	#print("Addition result was: " + addResult + ". The correct answer is: 1/1 or 2/2")
+
+	#subResult = fraction2 - fraction1
+	#print("Subtraction result was: " + subResult + ". The correct answer is: 0/0")
+
+	#multResult = fraction1 * fraction2
+	#print("Multiplication result was: " + multResult + ". The correct answer is: 1/4")
+
+	#divResult = fraction1 / fraction2
+	#print("Division result was: " + divResult + ". The correct answer is: 1/1")
