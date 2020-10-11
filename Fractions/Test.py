9a1604a3f92677cbb50b7559af400db2dbcb317c
KRHero03 <krunalhero@gmail.com>
2020-10-11 11:25:40 +0530

    Added Fraction Multiplication, Subtraction, Division and Improved Addition

--- Fractions/Test.py
+++ Fractions/Test.py
@@ -7,13 +7,13 @@ def main():
 	addResult = fraction1 + fraction2
 	print("Addition result was: " + str(addResult) + ". The correct answer is: 1/1 or 2/2")
 
-	#subResult = fraction2 - fraction1
-	#print("Subtraction result was: " + subResult + ". The correct answer is: 0/0")
+	subResult = fraction2 - fraction1
+	print("Subtraction result was: " + str(subResult) + ". The correct answer is: 0/0")
 
-	#multResult = fraction1 * fraction2
-	#print("Multiplication result was: " + multResult + ". The correct answer is: 1/4")
+	multResult = fraction1 * fraction2
+	print("Multiplication result was: " + str(multResult) + ". The correct answer is: 1/4")
 
-	#divResult = fraction1 / fraction2
-	#print("Division result was: " + divResult + ". The correct answer is: 1/1")
+	divResult = fraction1 / fraction2
+	print("Division result was: " + str(divResult) + ". The correct answer is: 1/1")
 
 main()
